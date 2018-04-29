3f37e1ef42ebcbf6dbc66233e693f9be72e61916
David Arelano <dtarellano1@gmail.com>
2018-04-29 14:03:40 -0700

    more webpack config and created example

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,46 @@
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './src/Uploadcare.vue',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    library: 'uploadcare-vue',
-    umdNamedDefine: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: __dirname,
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-env']
+var ROOT = path.resolve(__dirname)
+var root = path.join.bind(path, ROOT)
+
+const create = (filename, output, external) => {
+  return {
+    entry: './src/Uploadcare.vue',
+    output: {
+      path: output,
+      filename: filename,
+      libraryTarget: 'umd',
+      library: 'uploadcare-vue',
+      umdNamedDefine: true
+    },
+    externals: {
+      'uploadcare-widget': external
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: __dirname,
+          exclude: /node_modules/,
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.css$/,
+          loader: 'style-loader!less-loader!css-loader'
         }
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!less-loader!css-loader'
-      }
-    ]
-  },
-  plugins: [
-    // new webpack.optimize.UglifyJsPlugin( {
-    //   minimize : true,
-    //   sourceMap : false,
-    //   mangle: true,
-    //   compress: {
-    //     warnings: false
-    //   }
-    // })
-  ]
-};
+      ]
+    }
+  }
+}
+module.exports = [
+  create('index.js', root('./dist'), 'uploadcare-js'),
+  create('uploadcare-vue.js', root('./example'), 'uploadcare')
+];
